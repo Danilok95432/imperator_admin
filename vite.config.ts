c5b9c19d2f9f1f931ea4b8b781993afcdcb87f65
Danilok95432 <danila.dvoretskov@yandex.ru>
2026-03-06 15:53:45 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
 		},
 		proxy: {
 			'/api': {
-				target: 'https://auapi.npotau.ru/admin',
+				target: 'https://https://empapi.hayecke.org',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
 			},
